cb419a3daaa03121c3e21e0f824367824f49a619
KL44S <agustinbinci@hotmail.com>
2017-07-11 14:59:08 -0300

    Extensión soporta idioma inglés

--- js/popup.js
+++ js/popup.js
@@ -15,6 +15,60 @@ function saveValue(element) {
 	chrome.storage.sync.set(objectToPersist, function() {});
 }
 
+function eventFire(el, etype) {
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } 
+  else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
+function setLanguage(language) {
+	var title = document.getElementById("title");
+	var yearTitle = document.getElementById("yearTitle");
+	var votesTitle = document.getElementById("votesTitle");
+	var runtimeTitle = document.getElementById("runtimeTitle");
+	var from = document.getElementsByClassName("from");
+	var until = document.getElementsByClassName("until");
+
+	if (language == "esp") {
+		title.innerHTML = "FILTROS";
+		yearTitle.innerHTML = "A&#209;O";
+		votesTitle.innerHTML = "VOTOS";
+		runtimeTitle.innerHTML = "DURACION (min)";
+
+		for (var i = 0; i < from.length; i++) {
+			from[i].innerHTML = "desde";
+		}
+
+		for (var i = 0; i < until.length; i++) {
+			until[i].innerHTML = "hasta";
+		}
+	}
+	else {
+		title.innerHTML = "FILTERS";
+		yearTitle.innerHTML = "YEAR";
+		votesTitle.innerHTML = "VOTES";
+		runtimeTitle.innerHTML = "RUNTIME (min)";
+
+		for (var i = 0; i < from.length; i++) {
+			from[i].innerHTML = "from";
+		}
+
+		for (var i = 0; i < until.length; i++) {
+			until[i].innerHTML = "until";
+		}
+	}
+}
+
+function changeLanguage(language) {
+	chrome.storage.sync.set({languageSelect: language}, function() {});
+	setLanguage(language);
+}
+
 var fromYearSelectedValue = document.getElementById("fromYearSelectedValue");
 fromYearSelectedValue.addEventListener("change", function() {
 	saveValue(fromYearSelectedValue);
@@ -62,3 +116,32 @@ maxRuntimeSelectedValue.addEventListener("change", function() {
 	saveValue(maxRuntimeSelectedValue);
 });
 loadValue(maxRuntimeSelectedValue);
+
+var languageSelect = document.getElementById("myonoffswitch");
+languageSelect.addEventListener("change", function(setEnglish) {
+	chrome.storage.sync.get("languageSelect", function (data) {
+		var languageOption = data["languageSelect"];
+		var newLanguageOption = "esp";
+
+		if (languageOption != null && languageOption == "esp") {
+			newLanguageOption = "eng";
+		}
+		
+		changeLanguage(newLanguageOption);
+	});
+});
+
+chrome.storage.sync.get("languageSelect", function (data) {
+	var languageOption = data["languageSelect"];
+
+	if (languageOption != null) {
+		if (languageOption != "esp") {
+			languageSelect.removeAttribute("checked");
+		}
+
+		setLanguage(languageOption);
+	}
+	else {
+		chrome.storage.sync.set({languageSelect: "esp"}, function() {});
+	}
+});
